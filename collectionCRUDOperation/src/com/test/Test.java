317d2a5115f30d310cc3b6a2a4611cdb8fa6716a
srikanta338 <32513258+srikanta338@users.noreply.github.com>
2018-07-10 23:18:36 +0530

    adding in test class

--- collectionCRUDOperation/src/com/test/Test.java
+++ collectionCRUDOperation/src/com/test/Test.java
@@ -7,6 +7,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("okkkk");
 		ServiceImp s1=new ServiceImp();
 		//s1.getAllDetails(111);
 		//s1.getAllDetails(11);
